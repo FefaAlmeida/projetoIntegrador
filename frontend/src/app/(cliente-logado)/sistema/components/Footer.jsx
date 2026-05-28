9e3524b1893c95ec924cf558e66da5878a3475b4
Natalia <marchiori.nati30@gmail.com>
2026-05-28 11:24:53 -0300

    alteração no componente

---
+++ frontend/src/app/(cliente-logado)/sistema/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer
+      className="text-center py-4"
+      style={{
+        background: "#111",
+        color: "#999",
+      }}
+    >
+      © 2026 Luminar Solar Energy
+    </footer>
+  );
+}
